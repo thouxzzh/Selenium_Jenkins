73549fc6f3e64ff8228b9ac5c7531303500992c7
thouxzzh <2k21cse167@gmail.com>
2025-03-12 14:59:18 +0530

    updated Action class

---
+++ Selenium_Jenkins/src/main/java/com/sel/ActionClass.java
@@ -0,0 +1,28 @@
+package com.sel;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionClass {
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ChromeOptions options=new ChromeOptions();
+		WebDriver driver=new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.bstackdemo.com/");
+		WebElement ele=driver.findElement(By.xpath("//select"));
+		Actions action=new Actions(driver);
+		action.moveToElement(ele).perform();
+		WebElement customOption=driver.findElement(By.xpath("//option[text()='Lowest to highest']"));
+		customOption.click();
+		driver.close();
+		
+	}		
+}
